c7e7f7d25e341ce096cd7823cc8dc8afc135e267
brendanjlewis <brendan.j.f.lewis@gmail.com>
2024-02-25 19:01:30 -0500

    added some static metadata fields

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,8 +2,17 @@
 import "./globals.css";
 
 export const metadata = {
+  metadataBase: new URL('https://brendanjflewis.com'),
   title: "Brendan Lewis",
-  description: "Brendan's portfolio",
+  description: "Professional portfolio website for Brendan Lewis",
+  openGraph: {
+    title: 'Brendan Lewis',
+    description: 'Professional portfolio website for Brendan Lewis',
+    url: 'https://brendanjflewis.com/',
+    siteName: 'brendanjflewis',
+    locale: 'en_US',
+    type: 'website',
+  }
 };
 
 export default function RootLayout({ children }) {
